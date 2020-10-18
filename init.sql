186f8a07c04f90cdf1e0f9e6c2565fb43ccc5a3f
uhwGhGFaJd <uhwGhGFaJd@gmail.com>
2020-10-17 23:21:34 -0700

    add INSERT QUERY in init.sql.

---
+++ init.sql
@@ -0,0 +1,7 @@
+
+
+
+
+INSERT INTO config(id, config_name, config_value) VALUES(1, 'server_disabled', false);
+INSERT INTO config(id, config_name, config_value) VALUES(2, 'server_posting_disabled', false);
+INSERT INTO config(id, config_name, config_value) VALUES(3, 'server_manage_password', '$2a$10$VFgfzLUd1rtDGeyKL31MqOcyhk77uV.7fDMUraFqhL/W.Deejgaxi');
